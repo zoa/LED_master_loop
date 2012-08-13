c1aa10af502e7e2cc4d64271d3ebe190e4e331d7
sarah t <@>
2012-08-12 17:17:06 -0700

    adding strip update mode switching and transitions (not done)

--- LED_master_loop.ino
+++ LED_master_loop.ino
@@ -17,7 +17,11 @@ volatile unsigned long int interrupt_counter; // how many ms have elapsed since 
 unsigned long int prev_interrupt_counter; // the main loop uses this to detect when the interrupt counter has changed 
 unsigned int switch_after; // swap routines after this many milliseconds
 unsigned int active_routine; // matches the #s from the switch statement in the main loop
-void (*update)(); // pointer to current led-updating function
+void (*update)(); // pointer to current led-updating function within this sketch
+
+// pointer to a function in the Zoa_WS2801 library that takes a color argument. The update functions in this sketch use this
+// pointer to decide whether to call pushBack, pushFront or setAll.
+void (Zoa_WS2801::* library_update)(rgbInfo_t); 
 
 // Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
 Zoa_WS2801 strip = Zoa_WS2801(stripLen, dataPin, clockPin, WS2801_GRB);
@@ -37,14 +41,17 @@ void setup()
   strip.begin();
   strip.setAll(rgbInfo_t(0,0,0));
   
-  switch_after = 10000;
+  library_update = &Zoa_WS2801::pushFront;
+  
+  switch_after = 3000;
   interrupt_counter = switch_after + 1;
   prev_interrupt_counter = interrupt_counter;
   active_routine = 0;
   update = NULL;
   
-  // update the counter every millisecond
-  MsTimer2::set( 1, &update_interrupt_counter );
+  // update the interrupt counter (and thus the LEDs) every 30ms. The strip updating takes ~0.1ms 
+  // for each LED in the strip, and we are assuming a maximum strip length of 240, plus some extra wiggle room.
+  MsTimer2::set( 30, &update_interrupt_counter );
   MsTimer2::start();
 }
 
@@ -61,6 +68,22 @@ void loop()
     if ( i != active_routine )
     {
       deallocate_waveforms();
+      
+      // Decide how to show the current pattern (chasing up, chasing down, changing the whole strip at once )
+      byte update_func = random(0,2);
+      switch ( update_func )
+      {
+        case 1:
+          library_update = &Zoa_WS2801::pushBack;
+          break;
+        case 2:
+          library_update = &Zoa_WS2801::setAll;
+          break;
+        default:
+          library_update = &Zoa_WS2801::pushFront;
+      }
+      
+      // Decide which routine to show next
       switch (i)
       {
         case 0:
@@ -99,14 +122,23 @@ void loop()
         case 3:
           // mostly light blue/turquoise/purple with occasional bright green
           update = update_simple;
-          waves[0] = new Linear_generator( Linear_generator::TRIANGLE, 0, 30, 75 );
+          waves[0] = new Linear_generator( Linear_generator::TRIANGLE, 0, 30, 1, 75 );
           waves[1] = new Sine_generator( 30, 100, 1, 0 );
-          waves[2] = new Linear_generator( Linear_generator::TRIANGLE, 0, 100, 128 );
+          waves[2] = new Linear_generator( Linear_generator::TRIANGLE, 0, 100, 1, 128 );
           break;
+          /*
+        case 4:
+          // sets whole strip at once to blink between 2 different morphing purple-blues
+          // blinking is bad
+          update = update_all;
+          waves[0] = new Sine_generator( 0, 100, 2, 0 );
+          waves[1] = new Square_generator( 100, 200, 1, 100, 100, 1 );
+          waves[2] = new Square_generator( 100, 255, 1, 100, 100, 1 );
+          */
       }
       active_routine = i;
       interrupt_counter = 0;
-      //linear_transition();
+      linear_transition();
     }
   }
   // only update once every tick of the timer
@@ -124,7 +156,7 @@ void loop()
 
 void update_simple()
 {
-  strip.pushFront( get_next_rgb( waves[0], waves[1], waves[2] ) );
+  (strip.*library_update)( get_next_rgb( waves[0], waves[1], waves[2] ) );
   if ( !transitioning )
   {
     strip.show();
@@ -134,7 +166,7 @@ void update_simple()
 
 void update_convolved()
 {
-  strip.pushFront( rgbInfo( next_convolved_value(waves[0],waves[3]), next_convolved_value(waves[1],waves[4]), next_convolved_value(waves[2],waves[5]) ) );
+  (strip.*library_update)( rgbInfo( next_convolved_value(waves[0],waves[3]), next_convolved_value(waves[1],waves[4]), next_convolved_value(waves[2],waves[5]) ) );
   if ( !transitioning )
   {
     strip.show();
@@ -144,24 +176,21 @@ void update_convolved()
 
 //////// Transition functions //////////
 
-// This is messy and broken right now - needs to be debugged later
 void linear_transition()
 {  
   transitioning = true;
+  uint16_t pixel = (library_update == &Zoa_WS2801::pushBack) ? stripLen-1 : 0;
   // this is a total hack to get the first value of the next routine without actually displaying it (or having to change the update functions).
   // cache the current first value, update, grab the new first value, then reset the first pixel.
   // this will fall apart if the update routine updates all the pixels and not just the first one!!! check the transitioning flag in all
   // update functions to keep this from happening.
-  rgbInfo_t temp_first_value= strip.getPixelRGBColor(0);
+  rgbInfo_t temp_first_value = strip.getPixelRGBColor(pixel);
   update();
-  rgbInfo_t next_value = strip.getPixelRGBColor(0);
-  //strip.setPixelColor( 0, temp_first_value.r, temp_first_value.g, temp_first_value.b );
+  rgbInfo_t next_value = strip.getPixelRGBColor(pixel);
+  strip.setPixelColor( pixel, temp_first_value.r, temp_first_value.g, temp_first_value.b );
   transitioning = false;
-/*
-  linear_transition( rgbInfo_t(), rgbInfo_t(20,180,130), 200 );
-  linear_transition( rgbInfo_t(20,180,130), rgbInfo_t(), 200 );*/
-//linear_transition( rgbInfo_t(0,0,255), rgbInfo_t(0,255,255), 1000);
-linear_transition(temp_first_value,next_value,500);
+  
+  linear_transition(temp_first_value,next_value,500);
 }
 
 void linear_transition( const rgbInfo& start_value, const rgbInfo& target_value, uint16_t ms )
@@ -178,8 +207,7 @@ void linear_transition( const rgbInfo& start_value, const rgbInfo& target_value,
      transitional_value( start_value.g, target_value.g, multiplier ),
      transitional_value( start_value.b, target_value.b, multiplier )
      );    
-    strip.pushFront(c);
-    Serial.println(c.r);
+    (strip.*library_update)(c);
     strip.show();
   }
 }
@@ -200,7 +228,7 @@ float transitional_value( const float& from, const float& to, const float& multi
   return val;
 }
 
-// free the memory in the waves array
+// free the memory in the waves array and sets the update modes to 0
 void deallocate_waveforms()
 {
   for ( byte i = 0; i < WAVES; ++i )
